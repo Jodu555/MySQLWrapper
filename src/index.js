282e9dac7f2d2a09b10a34a753ff47179f38c7e8
Jodu555 <Jodu505@gmail.com>
2021-08-14 12:33:52 +0200

    Removed all of the test stuff

--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
 const Database = require("./Database");
-const db = Database.createDatabase('localhost', 'root', '', 'test');
-db.connect();
-// db.createTable('testtable', {
-//     'UUID': 'varchar(64)',
-//     'text': 'TEXT'
-// });
-// db.get('testtable').create({
-//     UUID: 'GREETINGS',
-//     text: 'from the nice application!'
-// });
-// (async () => {
-//     const one = await db.get('testtable').getOne({ UUID: 'GREETINGS' })
-//     console.log('One: ', one);
-//     const update = await db.get('testtable').update({ UUID: 'GREETINGS' }, { text: 'from the other nice application!' });
-//     console.log('Update: ', update);
-// })();
-// console.log(db.validate('testtable', { UUID: 'dsdsd', text: 'from the nice application!' }));
-
 
 module.exports = {
     Database,
