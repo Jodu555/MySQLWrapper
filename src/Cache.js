56763087e33bc1ca4a0c598c7181edab1349e052
Jodu555 <Jodu505@gmail.com>
2021-11-26 19:43:40 +0100

    added the whole get function

--- src/Cache.js
+++ src/Cache.js
@@ -19,4 +19,38 @@ class Cache {
     compareArray(arr1, arr2) {
         return arr1.length == arr2.length && JSON.stringify(arr1) == JSON.stringify(arr2);
     }
+
+    async get(...params) {
+        console.log('Params', params);
+        let returnObject;
+        let data = this.getValueFromMapAsArrayKey(params) || {};
+        if (this.calls) {
+            if (data && (data.calls <= this.calls)) {
+                //Persist
+                data.calls++;
+                returnObject = { ...returnObject, ...data, cached: true }
+            } else {
+                //Change
+                const result = await this.cb(...params);
+                data = { ...data, data: result, calls: 1 };
+                returnObject = { ...returnObject, ...data, cached: false }
+            }
+        }
+        if (this.cacheTime) {
+            if (data && (data.cacheTime >= Date.now())) {
+                //Persist
+                returnObject = { ...returnObject, ...data, cached: true };
+            } else {
+                //Change
+                const result = await this.cb(...params);
+                data = { ...data, data: result, cacheTime: Date.now() + this.cacheTime };
+                returnObject = { ...returnObject, ...data, cached: false };
+            }
+        }
+
+        this.map.set(params, data);
+
+        return returnObject;
+
+    }
 }
