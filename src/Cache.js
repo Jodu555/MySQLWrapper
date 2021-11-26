eb8f89a557f1bb8e4bab4e7f5fabec6f2f663319
Jodu555 <Jodu505@gmail.com>
2021-11-26 19:43:20 +0100

    added the necessary array methods

--- src/Cache.js
+++ src/Cache.js
@@ -6,4 +6,17 @@ class Cache {
         this.data = {};
         this.map = new Map();
     }
+
+    getValueFromMapAsArrayKey(arr) {
+        let retValue;
+        this.map.forEach((value, key) => {
+            if (this.compareArray(key, arr))
+                retValue = value;
+        });
+        return retValue;
+    }
+
+    compareArray(arr1, arr2) {
+        return arr1.length == arr2.length && JSON.stringify(arr1) == JSON.stringify(arr2);
+    }
 }
