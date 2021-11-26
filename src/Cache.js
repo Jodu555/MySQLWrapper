a690478b24d21a426933b5d49c4dd0941a1e061b
Jodu555 <Jodu505@gmail.com>
2021-11-26 19:43:00 +0100

    added the constructor

--- src/Cache.js
+++ src/Cache.js
@@ -1,3 +1,9 @@
 class Cache {
-
+    constructor(name, settings, cb) {
+        this.name = name;
+        ({ time: this.cacheTime, calls: this.calls } = settings);
+        this.cb = cb;
+        this.data = {};
+        this.map = new Map();
+    }
 }
