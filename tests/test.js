d2c39dcfcf91f0e2503865f7ba5419fa6ea98723
Jodu555 <Jodu505@gmail.com>
2022-03-06 10:19:16 +0100

    added my two test scripts so i dont have to use multiple vscodes

---
+++ tests/test.js
@@ -0,0 +1,215 @@
+// require('./test1');
+// return;
+
+const { Database } = require('../src/index');
+
+const database = Database.createDatabase('localhost', 'root', '', 'test');
+database.connect();
+
+
+
+
+const create_UUID = () => {
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
+}
+
+generateID = (len) => {
+    const poss = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    let id = '';
+    for (let i = 0; i < len; i++) {
+        id += poss[Math.floor(Math.random() * poss.length)];
+    }
+    return id;
+}
+
+const parseID = (val) => {
+    return { success: val.startsWith('==') && val.endsWith('0==='), errorMessage: 'The ID dont passes our system!' };
+}
+
+
+// //Default naming: createdAt: created_at | updatedAt: updated_at
+database.createTable('users', {
+    options: {
+        //IDEA
+        timestamps: true,
+        PK: 'UUID',
+        //Indexes: that a non primaty key should be created on that table
+        K: [
+            'username',
+            'password'
+        ],
+    },
+    'UUID': {
+        type: 'varchar(64)',
+        null: false,
+    },
+    'username': {
+        type: 'varchar(64)',
+        null: false,
+    },
+    'email': {
+        type: 'varchar(64)',
+        null: false,
+    },
+    'password': {
+        type: 'varchar(64)',
+        null: false,
+    }
+});
+
+
+const registerSchema = {
+    options: {
+        xor: ['username/email'],
+        even: ['password/repeatPassword'],
+        removeOthers: true, // This will disable theauto remove of all other values 
+    },
+    UUID: {
+        value: create_UUID,
+    },
+    username: {
+        anum: false,
+        min: 5, //Included
+        max: 25, //Included
+        default: 'Anonymous',
+    },
+    ID: {
+        required: true,
+        //TODO: Accept also an array of parse function for parse
+        parse: (val) => {
+            return { success: val.startsWith('==') && val.endsWith('0==='), errorMessage: 'The ID dont passes our system!' };
+        },
+    },
+    email: {
+        email: true
+    },
+    password: {
+        min: 3
+    },
+    repeatPassword: {},
+    fixedValue: {
+        value: '1231231'
+    },
+    generatedFixedValue: {
+        value: generateID(6)
+    },
+    state: {
+        required: true,
+        enum: ['activating', 'active', 'inactive']
+    }
+};
+
+const loginSchema = {
+    username: {
+        anum: false,
+        min: 5, //Included
+        max: 25, //Included
+        default: 'Anonymous',
+    },
+    email: {
+        email: true
+    },
+    password: {
+        min: 3
+    }
+};
+
+
+
+
+(async () => {
+
+
+    try {
+
+        const user = { username: 'Jodu555', email: 'editfusee@gmail.com', hack: '121212', ID: '==676760===', password: 'dsdsdsdsdsds', repeatPassword: 'dsdsdsdsdsds', state: 'activating' };
+        database.registerSchema('registerSchema', registerSchema, 'users');
+        database.registerSchema('loginSchema', loginSchema, 'users');
+
+
+        // const data = database.getSchema('registerSchema').validate(user, false)
+
+        // let validation = database.getSchema('registerSchema').validate(user, true); // true so it throws an error
+        // if (validation.success) {
+        //     console.log('Passed: ', validation.object);
+        // } else {
+        //     console.log(validation.errors);
+        // }
+
+        // validation = database.getSchema('loginSchema').validate({ username: 'Jodu555', email: 'editfusee@gmail.com', password: 'dsdsdsdsdsds' }, true); // true so it throws an error
+        // if (validation.success) {
+        //     console.log('Passed: ', validation.object);
+        // } else {
+        //     console.log(validation.errors);
+        // }
+
+
+
+    } catch (error) {
+        if (error instanceof database.ParsingError) {
+            console.log('Parsing Error: ' + error.message);
+            //Do whatever you want
+        } else {
+            console.log(error);
+        }
+
+    }
+    // console.log(database.tables.get('users'));
+
+    function registerSchemaDir(name, object) {
+        schems.set(name, object);
+        console.log('Registered Schema for ' + name);
+        // if (!(name && object)) {
+        //     object = name;
+        //     const re = /registerSchema\(\S.*\)(?! \{)/g
+        //     const match = arguments.callee.caller.toString().match(re);
+        //     for (const value of match) {
+        //         innerName = value.split('(')[1].split(')')[0];
+        //         if (!innerName.includes(',') && !schems.get(innerName)) {
+        //             name = innerName
+        //             break;
+        //         }
+        //     }
+        // }
+    }
+
+
+    const obj = {
+        username: 'Jodu555',
+        password: 'asasasasas'
+    };
+
+    Object.defineProperty(obj, 'UUID', {
+        value: 'axaxaxaxaxaxaxax',
+        writable: false,
+        enumerable: false,
+        configurable: false
+    });
+
+    console.log(obj, obj.UUID);
+
+    // console.log(obj);
+
+    // obj.UUID = 'xdxdxdxd';
+    // console.log(obj);
+
+    // setInterval(async () => {
+    //     const user = await database.get('users').getLatest('updated');
+    //     console.log(user);
+    // }, 5000);
+
+
+    // const user = await database.get('users')
+    // const user = await database.get('users').getLatest('updated');
+    // const user = await database.get('users').getLatest('deleted');
+    // const user = await database.get('users').getLatestInserted({ UUID: 'test' });
+    // const user2 = await database.get('users').getLatest({ UUID: 'test' });
+
+})();
+
---
+++ tests/test1.js
@@ -0,0 +1,170 @@
+// const { Database } = require('mysqlapi');
+
+// const test1 = {
+//     softdelete: true,
+//     timestamps: true,
+// }
+
+// const test2 = {
+//     softdelete: true,
+//     timestamps: {
+//         createdAt: true,
+//         updatedAt: false,
+//         deletedAt: true,
+//     },
+// }
+
+// const test3 = {
+//     timestamps: {
+//         createdAt: 'created_at',
+//         updatedAt: 'updated_at',
+//     },
+// }
+
+// const test4 = {
+//     timestamps: {
+//         createdAt: 'created_at',
+//         updatedAt: 'updated_at',
+//     },
+// }
+
+// console.log(parse(test1));
+// console.log(parse(test2));
+// console.log(parse(test3));
+
+//Enable All with default naming
+// timestamps: true,
+//Enable only one or two with default naming
+// timestamps: {
+//     cratedAt: true,
+//     updatedAt: true,
+// },
+//Enable only one or two with custom colum naming
+// timestamps: {
+//     cratedAt: 'created_at',
+//     updatedAt: 'updated_at',
+// },
+
+// function parse(options) {
+//     const CREATE_COLUM_NAME = 'created_at';
+//     const UPDATE_COLUM_NAME = 'updated_at';
+//     const DELETE_COLUM_NAME = 'deleted_at';
+//     let output = {};
+//     const timestamps = options.timestamps;
+
+//     const stamps = new Map([
+//         ['createdAt', 'created_at'],
+//         ['updatedAt', 'updated_at']
+//     ]);
+
+//     if (typeof timestamps === 'boolean') {
+//         output.createdAt = CREATE_COLUM_NAME;
+//         output.updatedAt = UPDATE_COLUM_NAME;
+//         if (options.softdelete)
+//             output.deletedAt = DELETE_COLUM_NAME;
+//     } else {
+//         stamps.forEach((value, key) => (typeof timestamps[key] === 'boolean' && timestamps[key]) ? output[key] = value : '');
+//         if (options.softdelete && typeof timestamps.deletedAt === 'boolean' && timestamps.deletedAt) {
+//             output.deletedAt = DELETE_COLUM_NAME;
+//         }
+//         stamps.forEach((value, key) => (typeof timestamps[key] === 'string') ? output[key] = value : '');
+//         if (options.softdelete && typeof timestamps.deletedAt === 'string') {
+//             output.deletedAt = timestamps.deletedAt;
+//         }
+//     }
+//     return output;
+// }
+
+// const callbacks = new Map();
+
+// callbacks.set('tablename-ACTION', 'test1');
+// callbacks.set('test-ACTION', 'test1.1');
+// callbacks.set('*-ACTION', 'test2');
+// callbacks.set('*-TEDT', 'test2.1');
+// callbacks.set('*-*', ['test3.1', 'test3.2']);
+
+// console.log(
+//     callCallback('tablename', 'ACTION', {}),
+// );
+
+// function callCallback(tablename, action, data) {
+//     const callbacks = callbackToFunctions(tablename, action).filter(v => typeof v == 'function');
+//     callbacks.forEach(callback => {
+//         callback(data)
+//     });
+// }
+
+// function callbackToFunctions(tablename, action) {
+//     if (!tablename || !action) {
+//         return [];
+//     }
+//     const possibles = [];
+//     possibles.push(callbacks.get('*-*'));
+//     possibles.push(callbacks.get('*-' + action));
+//     possibles.push(callbacks.get(tablename + '-*'));
+//     possibles.push(callbacks.get(tablename + '-' + action));
+
+//     const otherArrays = [];
+//     const possiblesCleaned = possibles.map(v => {
+//         if (!Array.isArray(v))
+//             return v;
+//         otherArrays.push(v);
+//         return undefined;
+//     }).filter(v => typeof v !== 'undefined');
+//     var merged = [].concat.apply([], otherArrays);
+//     const output = merged.concat(possiblesCleaned);
+//     return output;
+// }
+
+// function msToTime(duration) {
+//     var milliseconds = parseInt((duration % 1000))
+//         , seconds = parseInt((duration / 1000) % 60)
+//         , minutes = parseInt((duration / (1000 * 60)) % 60)
+//         , hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+//     let output = '';
+//     if (hours !== 0)
+//         output += `${hours} ${hours > 1 ? 'Stunden' : 'Stunde'} `
+//     if (minutes !== 0)
+//         output += `${minutes} ${minutes > 1 ? 'Minuten' : 'Minute'} und `
+//     if (seconds !== 0)
+//         output += `${seconds} ${seconds > 1 ? 'Sekunden' : 'Sekunde'}`
+//     return output;
+// }
+
+// const { Database } = require('mysqlapi');
+// const database = Database.getDatabase();
+
+// database.get('users').actions.log('from other ding')
+
+
+const wait = (m) => new Promise((r, j) => setTimeout(r, m));
+
+const cbs = [];
+
+regCallback(async (context, next) => {
+    console.log(1);
+    await wait(1000)
+    context.name = 'Roberto';
+    next();
+})
+
+regCallback((context, next) => {
+    console.log(2);
+    context.name = [context.name, 'Alejandro'];
+    next();
+})
+
+callCallback({}).then(console.log);
+
+
+function regCallback(cb) {
+    cbs.push(cb);
+}
+
+async function callCallback(data) {
+    for (const x of cbs) {
+        await x(data, () => { });
+    }
+    return data;
+}
